e2282f3804fa1f9f505b68482e88080a54ecdec2
Анна Ковальчук <144364193+anetkovalchuk@users.noreply.github.com>
2025-12-09 20:33:15 +0200

    Add B0101 class to greet user by name

---
+++ В01-01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B0101 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введіть ім’я: ");
+        String name = in.nextLine();
+
+        System.out.println("Привіт, " + name + "!");
+    }
+}
