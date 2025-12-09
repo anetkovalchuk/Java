a0a7c58de9cf676cc28afcc819d24ae93af4712a
Анна Ковальчук <144364193+anetkovalchuk@users.noreply.github.com>
2025-12-09 20:34:43 +0200

    Add B0102 class to print arguments in reverse order

---
+++ B01-02.java
@@ -0,0 +1,15 @@
+public class B0102 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Аргументів немає.");
+            return;
+        }
+
+        System.out.println("Аргументи у зворотному порядку:");
+
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.println(args[i]);
+        }
+    }
+}
